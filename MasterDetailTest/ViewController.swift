d9182f878ce4097e20c82d434297b4fec82d1a68
javieraeromexico <japaeza@aeromexico.com>
2025-02-06 17:05:40 -0600

    Initial Commit

---
+++ MasterDetailTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MasterDetailTest
+//
+//  Created by Javier Apaez Arcos on 06/02/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MasterDetailTestTests/MasterDetailTestTests.swift
@@ -0,0 +1,17 @@
+//
+//  MasterDetailTestTests.swift
+//  MasterDetailTestTests
+//
+//  Created by Javier Apaez Arcos on 06/02/25.
+//
+
+import Testing
+@testable import MasterDetailTest
+
+struct MasterDetailTestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
